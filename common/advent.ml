f7e6dc2504a399e5c2b6694653a12ae932fa574b
Leif Metcalf <me@leif.nz>
2022-12-02 11:42:32 +1300

    Add prime sieve

---
+++ common/advent.ml
@@ -0,0 +1,15 @@
+open Core
+
+let primes n =
+  let arr = Array.create ~len:(n + 1) true in
+  let res = ref [] in
+  for i = 2 to n do
+    if arr.(i) then
+      res := i :: !res;
+      let j = ref (i + i) in
+      while !j <= n do
+        arr.(!j) <- false;
+        j := !j + i
+      done
+  done;
+  List.rev !res

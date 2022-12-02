5894378dc29aebe8b1c8b29a372b176637da35e3
Leif Metcalf <me@leif.nz>
2022-12-03 11:01:58 +1300

    Rewrite day 4 as a fold

--- days/day2.ml
+++ days/day2.ml
@@ -1,12 +1,11 @@
 open Core
 
 let () =
-  let parse_line line = match String.to_list line with
-    | [a; _; b] -> (Char.to_int a - 65, Char.to_int b - 88)
-    | _ -> failwith "parse failed"
+  let f (acc1, acc2) line =
+    let a = Char.to_int line.[0] - 65 in
+    let b = Char.to_int line.[2] - 88 in
+    (acc1 + (b - a + 1) % 3 * 3 + b + 1,
+     acc2 + b * 3 + (a + b - 1) % 3 + 1)
   in
-  let lines = List.rev_map (In_channel.input_lines In_channel.stdin) ~f:parse_line in
-  let score (a, b) = (b - a + 1) % 3 * 3 + b + 1 in
-  printf "Part 1: %d\n" @@ List.fold lines ~init:0 ~f:(fun b a -> b + score a);
-  let massage (a, b) = (a, (a + b - 1) % 3) in
-  printf "Part 2: %d\n" @@ List.fold lines ~init:0 ~f:(fun b a -> b + score (massage a))
+  let (part1, part2) = In_channel.fold_lines In_channel.stdin ~init:(0, 0) ~f in
+  printf "Part 1: %d\nPart 2: %d\n" part1 part2

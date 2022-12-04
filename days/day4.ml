c63c7f0d0ac797239c435abc0ae36aea594882b9
Leif Metcalf <me@leif.nz>
2022-12-04 18:28:17 +1300

    Simplify day 4

--- days/day4.ml
+++ days/day4.ml
@@ -2,13 +2,11 @@ open Core
 
 let parse_line line =
   match List.map ~f:int_of_string @@ String.split_on_chars ~on:['-'; ','] line with
-    | [a; b; c; d] -> ((a, b), (c, d))
+    | [a; b; c; d] -> (a, b, c, d)
     | _ -> failwith "parse error"
 
 let () =
   let pairs = List.map ~f:parse_line @@ In_channel.input_lines In_channel.stdin in
-  let f acc ((a, b), (c, d)) = if a <= c && d <= b || c <= a && b <= d then acc + 1 else acc in
-  let part_1 = List.fold ~f ~init:0 pairs in
-  let f acc ((a, b), (c, d)) = if Int.max a c <= Int.min b d then acc + 1 else acc in
-  let part_2 = List.fold ~f ~init:0 pairs in
+  let part_1 = List.count pairs ~f:(fun (a, b, c, d) -> a <= c && d <= b || c <= a && b <= d) in
+  let part_2 = List.count pairs ~f:(fun (a, b, c, d) -> Int.max a c <= Int.min b d) in
   printf "Part 1: %d\nPart 2: %d\n" part_1 part_2

170867a19a04747f718c74c92213f7a926ded754
Leif Metcalf <me@leif.nz>
2022-12-05 20:26:03 +1300

    Replace list with array

--- days/day5.ml
+++ days/day5.ml
@@ -11,7 +11,7 @@ let parse_input lines =
     | (x :: rest) when Char.is_digit x -> Some (List.drop_while ~f:(fun c -> Char.(c = ' ')) @@ List.rev rest)
     | _ -> None
   in
-  let stacks = List.filter_map ~f @@ Option.value_exn @@ List.transpose @@ List.rev_map ~f:String.to_list stacks in
+  let stacks = Array.of_list @@ List.filter_map ~f @@ Option.value_exn @@ List.transpose @@ List.rev_map ~f:String.to_list stacks in
   let f line = match String.split ~on:' ' line with
     | [_; n; _; a; _; b] -> (int_of_string n, int_of_string a - 1, int_of_string b - 1)
     | _ -> failwith "invalid move"
@@ -22,19 +22,13 @@ let parse_input lines =
 let () =
   let (stacks, moves) = parse_input @@ In_channel.input_lines In_channel.stdin in
   let solve rev =
-    let f stacks (n, a, b) =
-      let f i stack =
-        if i = a then
-          List.drop stack n
-        else if i = b then
-          List.append (rev (List.take (List.nth_exn stacks a) n)) stack
-        else
-          stack
-      in
-      List.mapi ~f stacks
+    let stacks = Array.copy stacks in
+    let f (n, a, b) =
+      let (top, bottom) = List.split_n stacks.(a) n in
+      stacks.(a) <- bottom;
+      stacks.(b) <- List.append (rev top) stacks.(b)
     in
-    let final_stacks = List.fold ~f ~init:stacks moves in
-    let tops = List.map ~f:(fun stack -> List.nth_exn stack 0) final_stacks in
-    String.of_char_list tops
+    List.iter ~f moves;
+    String.of_char_list @@ Array.to_list @@ Array.map ~f:List.hd_exn stacks
   in
   printf "Part 1: %s\nPart 2: %s\n" (solve List.rev) (solve Fun.id)

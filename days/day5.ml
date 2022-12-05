db70ccbd5ca4a41698948141b65b0d6fb3647f68
Leif Metcalf <me@leif.nz>
2022-12-05 20:28:36 +1300

    Make array use internal to solve function

--- days/day5.ml
+++ days/day5.ml
@@ -11,7 +11,7 @@ let parse_input lines =
     | (x :: rest) when Char.is_digit x -> Some (List.drop_while ~f:(fun c -> Char.(c = ' ')) @@ List.rev rest)
     | _ -> None
   in
-  let stacks = Array.of_list @@ List.filter_map ~f @@ Option.value_exn @@ List.transpose @@ List.rev_map ~f:String.to_list stacks in
+  let stacks = List.filter_map ~f @@ Option.value_exn @@ List.transpose @@ List.rev_map ~f:String.to_list stacks in
   let f line = match String.split ~on:' ' line with
     | [_; n; _; a; _; b] -> (int_of_string n, int_of_string a - 1, int_of_string b - 1)
     | _ -> failwith "invalid move"
@@ -22,7 +22,7 @@ let parse_input lines =
 let () =
   let (stacks, moves) = parse_input @@ In_channel.input_lines In_channel.stdin in
   let solve rev =
-    let stacks = Array.copy stacks in
+    let stacks = Array.of_list stacks in
     let f (n, a, b) =
       let (top, bottom) = List.split_n stacks.(a) n in
       stacks.(a) <- bottom;
